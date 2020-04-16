d9b4d8dc3e902c3d13d96f4eb9c58f581901f49a
Justin <witol.justin@hotmail.com>
2020-04-16 16:29:58 -0400

    More Changes

--- MoonRetriever.cs
+++ MoonRetriever.cs
@@ -95,22 +95,20 @@ namespace Dx2_DiscordBot
                     }
 
                     var role = g.Roles.First(r => r.Name == "FullMoonCrew");
-                    var description = "";
-                    
-                    description = "Full Moon has started in Aura Gate!";
-
                     var eb = new EmbedBuilder();
-                    eb.WithDescription(description);
+                    eb.WithDescription("Full Moon has started in Aura Gate!");
 
                     if (moonPhase && moonPhaseChnlId != 0)
                     {
                         var chnl = _client.GetChannel(moonPhaseChnlId) as IMessageChannel;
+                        if (role != null && role.IsMentionable)
+                            await chnl.SendMessageAsync(role.Mention);
                         await chnl.SendMessageAsync("", false, eb.Build());
                     }
                     else if (botSpam && botSpamChnlId != 0)
                     {
                         var chnl = _client.GetChannel(botSpamChnlId) as IMessageChannel;
-                        if (role != null)
+                        if (role != null && role.IsMentionable)
                             await chnl.SendMessageAsync(role.Mention);
                         await chnl.SendMessageAsync("", false, eb.Build());
                     }
@@ -189,11 +187,13 @@ namespace Dx2_DiscordBot
             return nextFullMoon;
         }
 
+        //Gets next full moon by adding seconds to time provided
         public int GetNextFullMoon(int currentTime)
         {
             return currentTime + full_moon_duration * 2 + moon_duration * 14;
         }
 
+        //Converts seconds to time with seconds beginning at 1/1/1970
         public string FormatNextTime(int seconds)
         {            
             var moonTime = new DateTime(1970, 1, 1).AddSeconds(seconds);
@@ -204,127 +204,13 @@ namespace Dx2_DiscordBot
             return moonTime.ToString("H:mm tt UTC") + string.Format(" {0} Hour(s) and {1} Minute(s) Away", timeSpan.Hours, timeSpan.Minutes);
         }
 
-        public string FormatAlertTime(int seconds)
-        {
-            var moonTime = new DateTime(1970, 1, 1).AddSeconds(seconds);
-            var now = DateTime.Now.ToUniversalTime();
-
-            var timeSpan = moonTime.Subtract(now);
-
-            return moonTime.ToString("H:mm tt UTC") + string.Format(" {0} Hour(s) and {1} Minute(s) Away", timeSpan.Hours, timeSpan.Minutes);
-        }
-
+        //Gets seconds since 1/1/1970
         public double GetCurrentTime()
         {
             var time = (DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1));
             return Math.Floor(time.TotalMilliseconds / 1000);
         }
         
-        //source
-
-        //function formatTime(timestamp)
-        //{
-        //    return moment(timestamp * 1000).format('llll')
-        //}
-
-        //function getCurrentTimestamp()
-        //{
-        //    return Math.floor(new Date().getTime() / 1000.0)
-        //}
-
-        //const START_REF = 1544848380
-        //const MOON_DURATION = 7 * 60
-        //const FM_DURATION = 10 * 60
-        //const NEXT_MOONS_COUNT = 20
-
-        //function getNextFullMoon(last_moon)
-        //    {
-        //        return last_moon + FM_DURATION * 2 + MOON_DURATION * 14
-        //}
-
-        //    function getUpcomingMoon(start_ref, current_time)
-        //    {
-        //        var current_time = getCurrentTimestamp()
-        //      var next_full_moon = getNextFullMoon(start_ref)
-        //      if (next_full_moon < current_time)
-        //        {
-        //            return getUpcomingMoon(next_full_moon)
-        //  }
-        //        return next_full_moon
-        //    }
-
-        //    function getNextNFullMoons(start_ref, moons, count)
-        //    {
-        //        if (moons.length < count)
-        //        {
-        //            var last_moon = moons[moons.length - 1] || start_ref
-        //          var next_moon = getNextFullMoon(last_moon)
-        //          moons.push(next_moon)
-        //          return getNextNFullMoons(last_moon, moons, count)
-        //        }
-        //        return moons
-        //    }
-
-        //    function getOpenDuration(timestamp)
-        //    {
-        //        var minutes = moment(timestamp * 1000).minutes()
-        //      var open_minutes = FM_DURATION / 60
-
-        //  if (minutes + open_minutes >= 60)
-        //        {
-        //            return Math.max((55 - minutes), 0) + ((minutes + open_minutes) - 60)
-        //  }
-        //        return Math.min(55 - minutes, open_minutes)
-        //    }
-
-        //    function insertTableMoons(table_ref, moon_time)
-        //    {
-        //        var new_row = table_ref.insertRow(table_ref.rows.length)
-
-        //  var time_cell = new_row.insertCell(0)
-        //      time_cell.appendChild(document.createTextNode(formatTime(moon_time)))
-
-        //  var duration_cell = new_row.insertCell(1)
-        //      var duration = getOpenDuration(moon_time)
-        //      if (duration * 60 < FM_DURATION)
-        //        {
-        //            duration_cell.className = 'shitMoon'
-        //  }
-        //        duration_cell.appendChild(document.createTextNode(duration))
-        //    }
-
-        //    var next_full_moon = getUpcomingMoon(START_REF)
-        //document.getElementById('nextFM').textContent = formatTime(next_full_moon)
-
-        //var table_ref = document.getElementById('nextMoonsTable').getElementsByTagName('tbody')[0]
-        //var next_moons = getNextNFullMoons(next_full_moon, [], NEXT_MOONS_COUNT)
-        //next_moons.forEach(function(moon_time) {
-        //  insertTableMoons(table_ref, moon_time)
-        //})
-
-        //function formatCountDown(h, m, s)
-        //{
-        //    return (
-        //      h.toString().padStart(2, '0') +
-        //      ':' +
-        //      m.toString().padStart(2, '0') +
-        //      ':' +
-        //      s.toString().padStart(2, '0')
-        //    )
-        //}
-
-        //function updateCounter()
-        //{
-        //    var countdown = Math.max(next_full_moon - getCurrentTimestamp(), 0)
-        //  var duration = moment.duration(countdown, 'seconds')
-        //  var display = formatCountDown(duration.hours(), duration.minutes(), duration.seconds())
-        //  document.getElementById('countDown').textContent = display
-        //}
-        //updateCounter()
-        //setInterval(updateCounter, 1000)
-
-
-
         #endregion
     }
 }

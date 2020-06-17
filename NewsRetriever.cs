a3c283f736e8f5f7ad3c19f368a30530e4cd78da
Alenael <46180324+Alenael@users.noreply.github.com>
2020-06-17 17:37:05 -0400

    Updates to News Stuff

--- NewsRetriever.cs
+++ NewsRetriever.cs
@@ -54,7 +54,7 @@ namespace Dx2_DiscordBot
                 {
                     if (_client.GetChannel(channelId) is IMessageChannel chnl)
                     {
-                        _ = SendNews(new News() { Title = "Kishin/Snake Fusion 30% OFF Event Coming Soon! (This is a manual test of the system. Thanks for the understanding.)", Url = "https://d2-megaten-l.sega.com/en/news/detail/079492.html", Image = "https://d2-megaten-l.sega.com/webview/en/upload_images/848669ee2d7bc251692a9f264817eb62cc7ee313.png" }, true);
+                        _ = SendNews(new News() { Title = "Kishin/Snake Fusion 30% OFF Event Coming Soon! (This is a manual test of the system. Thanks for the understanding.)", Url = "https://d2-megaten-l.sega.com/en/news/detail/079492.html", Image = "https://d2-megaten-l.sega.com/webview/en/upload_images/848669ee2d7bc251692a9f264817eb62cc7ee313.png" }, 0, true);
                     }
                 }
             }
@@ -84,13 +84,16 @@ namespace Dx2_DiscordBot
 
             var newNewsItems = await GetNewsItems();
 
+            var count = 0;
+
             foreach (var ni in newNewsItems)
             {
                 if (!NewsItems.Any(i => i.Url == ni.Url))
                 {
                     _ = Logger.LogAsync($"New News Posted! {ni.Title}");
-                    _ = SendNews(ni);
+                    _ = SendNews(ni, count);
                     NewsItems.Add(ni);
+                    count = count++;
                 }
             }
 
@@ -100,84 +103,89 @@ namespace Dx2_DiscordBot
         private List<string> botSpamChannelNames = new List<string>() { "bot-spam", "spam-bot" };
         private List<string> newsChannelNames = new List<string>() { "news", "announcements" };
 
-        public async Task SendNews(News ni, bool testing = false)
+        public async Task SendNews(News ni, int count, bool testing = false)
         {
-            foreach (var g in _client.Guilds)
+            var fileName = $"newsbanner_{count}.png";
+
+            using (System.Net.WebClient client = new System.Net.WebClient())
             {
-                var newsChn = false;
-                var botSpam = false;
+                if (ni.Image != null)
+                    client.DownloadFile(new Uri(ni.Image), fileName);
 
-                ulong botSpamChnlId = 0;
-                ulong newsChnlId = 0;
-
-                foreach (var c in g.Channels)
+                foreach (var g in _client.Guilds)
                 {
-                    if (!botSpam)
+                    var newsChn = false;
+                    var botSpam = false;
+
+                    ulong botSpamChnlId = 0;
+                    ulong newsChnlId = 0;
+
+                    foreach (var c in g.Channels)
                     {
-                        botSpam = botSpamChannelNames.Any(s => s == c.Name);
-                        if (botSpam)
-                            botSpamChnlId = c.Id;
-                    }
-                    if (!newsChn)
-                    {
-                        newsChn = newsChannelNames.Any(s => s == c.Name);
-                        if (newsChn)
-                            newsChnlId = c.Id;
-                    }
-                }
-
-                SocketTextChannel chnl = null;
-                bool canSend = false;
-
-                if (newsChn && newsChnlId != 0)
-                {
-                    chnl = _client.GetChannel(newsChnlId) as SocketTextChannel;
-                    if (chnl != null)
-                        canSend = g.CurrentUser.GetPermissions(chnl).SendMessages &&
-                            g.CurrentUser.GetPermissions(chnl).EmbedLinks &&
-                            g.CurrentUser.GetPermissions(chnl).AttachFiles;
-                }
-                if (!canSend && botSpam && botSpamChnlId != 0)
-                {
-                    chnl = _client.GetChannel(botSpamChnlId) as SocketTextChannel;
-                    if (chnl != null)
-                        canSend = g.CurrentUser.GetPermissions(chnl).SendMessages &&
-                            g.CurrentUser.GetPermissions(chnl).EmbedLinks &&
-                            g.CurrentUser.GetPermissions(chnl).AttachFiles;
-                }
-
-                if (chnl != null && canSend)
-                {
-                    try
-                    {
-                        if (ni.Image != null)
+                        if (!botSpam)
                         {
-                            using (System.Net.WebClient client = new System.Net.WebClient())
+                            botSpam = botSpamChannelNames.Any(s => s == c.Name);
+                            if (botSpam)
+                                botSpamChnlId = c.Id;
+                        }
+                        if (!newsChn)
+                        {
+                            newsChn = newsChannelNames.Any(s => s == c.Name);
+                            if (newsChn)
+                                newsChnlId = c.Id;
+                        }
+                    }
+
+                    SocketTextChannel chnl = null;
+                    bool canSend = false;
+
+                    if (newsChn && newsChnlId != 0)
+                    {
+                        chnl = _client.GetChannel(newsChnlId) as SocketTextChannel;
+                        if (chnl != null)
+                            canSend = g.CurrentUser.GetPermissions(chnl).SendMessages &&
+                                g.CurrentUser.GetPermissions(chnl).EmbedLinks &&
+                                g.CurrentUser.GetPermissions(chnl).AttachFiles;
+                    }
+                    if (!canSend && botSpam && botSpamChnlId != 0)
+                    {
+                        chnl = _client.GetChannel(botSpamChnlId) as SocketTextChannel;
+                        if (chnl != null)
+                            canSend = g.CurrentUser.GetPermissions(chnl).SendMessages &&
+                                g.CurrentUser.GetPermissions(chnl).EmbedLinks &&
+                                g.CurrentUser.GetPermissions(chnl).AttachFiles;
+                    }
+
+                    if (chnl != null && canSend)
+                    {
+                        try
+                        {
+                            if (ni.Image != null)
                             {
-                                var ext = ".png";
-                                client.DownloadFile(new Uri(ni.Image), "newsbanner" + ext);
-                                if (File.Exists("newsbanner.png"))
-                                {
-                                    await chnl.SendFileAsync("newsbanner" + ext, $"**{ni.Title}**\n{ni.Url}");
-                                    File.Delete("newsbanner.png");
-                                }
+                                if (File.Exists(fileName))
+                                    await chnl.SendFileAsync(fileName, $"**{ni.Title}**\n{ni.Url}");
                                 else
-                                    await chnl.SendMessageAsync($"**\n{ni.Url}");
+                                    await chnl.SendMessageAsync($"**{ni.Title}**\n{ni.Url}");
                             }
-                        }
-                        else
-                        {
-                            await chnl.SendMessageAsync($"**\n{ni.Url}");
-                        }
+                            else
+                            {
+                                await chnl.SendMessageAsync($"**\n{ni.Url}");
+                            }
 
-                        await Logger.LogAsync("Sending News to '" + g.Name + "' in channel '" + chnl.Name + "'");
+                            await Logger.LogAsync("Sending News to '" + g.Name + "' in channel '" + chnl.Name + "'");
+                        }
+                        catch (Exception e)
+                        {
+                            await Logger.LogAsync("Could not send to '" + g.Name + "' in channel '" + chnl.Name + "'");
+                        }
                     }
-                    catch(Exception e)
-                    {
-                        await Logger.LogAsync("Could not send to '" + g.Name + "' in channel '" + chnl.Name + "'");
-                    }
+
+                    
                 }
             }
+
+            if (ni.Image != null)
+                File.Delete(fileName);
         }
 
         public async Task<List<News>> GetNewsItems()
